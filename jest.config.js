8f152bb27eddde6b464031199d730a8773b9abe8
nauris-m <nauris.m@gmail.com>
2022-01-27 16:51:56 +0200

    init

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  projects: [
+    '<rootDir>/apps/shell',
+    '<rootDir>/apps/menu',
+    '<rootDir>/apps/mtg-singles',
+    '<rootDir>/apps/mtg-boosters',
+    '<rootDir>/apps/third',
+  ],
+};
